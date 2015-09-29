1af72b776b038301ed3dcbcf56658e225028b9cc
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-09-29 09:37:51 +0900

    update

--- goji.go
+++ goji.go
@@ -13,6 +13,10 @@ func hello(c web.C, w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+        goji.Get("/bind/:name", hello)
+        goji.Get("/yuck/:name", hello)
+        goji.Get("/none/:name", hello)
+        goji.Get("/bye/:name", hello)
         goji.Get("/hello/:name", hello)
         goji.Serve()
 }
